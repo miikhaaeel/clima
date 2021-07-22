90a1aaa63636c88bd9f3be4c302f533a08728455
Miikhaaeel <fadowomichael1@gmail.com>
2021-07-22 12:54:30 +0100

    created getData method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 import 'location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -12,6 +12,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -20,6 +21,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    //var url = Uri.https('api.openweathermap.org', '/data/2.5/weather');
+
+    var response = await get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e7aac0565bddb97869fe722a66a8457c'));
+    print(response.statusCode);
+    print(response.body);
+    //https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e7aac0565bddb97869fe722a66a8457c
   }
 
   @override
