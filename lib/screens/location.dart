9842b2911d21f60d85f31bbdf0888d397641ed79
Miikhaaeel <fadowomichael1@gmail.com>
2021-07-30 23:24:45 +0100

    refactored location.dart

--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -3,7 +3,7 @@ import 'package:geolocator/geolocator.dart';
 class Location {
   double? latitude;
   double? longitude;
-   //Location({this.latitude, this.longitude});
+
 
   Future<void> getCurrentLocation() async {
     try {
