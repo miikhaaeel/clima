93e96067b1e23d8d4e31e361ca8814291f2da553
Miikhaaeel <fadowomichael1@gmail.com>
2021-07-31 02:54:19 +0100

    refactored weather.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/networking.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
-
-import 'location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,7 +9,7 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  String apiKey = 'e7aac0565bddb97869fe722a66a8457c';
+  final weather = WeatherModel();
   @override
   void initState() {
     super.initState();
@@ -19,27 +17,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    var networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
-    var temperature = weatherData['main']['temp'];
-    var condition = weatherData['weather'][0]['id'];
-    var cityName = weatherData['name'];
-
-    print(temperature);
-    print(condition);
-    print(cityName);
-
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) {
           return LocationScreen(
-            locationWeather: weatherData,
+            locationWeather: weather.getLocationWeather(),
           );
         },
       ),
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,22 @@
+import 'package:clima/screens/location.dart';
+import 'package:clima/services/networking.dart';
+
+String apiKey = 'e7aac0565bddb97869fe722a66a8457c';
+const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  final location = Location();
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    var networkHelper = NetworkHelper(
+        '$openWeatherMapUrl?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
